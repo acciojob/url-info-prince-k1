d64f81611e060487287bf33dc6076057fa32af88
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-11 17:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const currentUrl = window.location.href;
+let str = currentUrl.toLowerCase().split('');
+
+let result = '';
+for(let i = 0; i < str.length; i++){
+    if(str[i].charCodeAt(0) >= 97 && str[i].charCodeAt(0) <= 122){
+        result += str[i];
+    };
+}
+alert(`The length of the URL is : ${result.length}`);
